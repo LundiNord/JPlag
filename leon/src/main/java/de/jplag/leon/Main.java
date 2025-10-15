c361278c9952700d1a1eb2ea74b9c5e24bf0f1c7
Emi <git@nyxnord.de>
2025-10-15 12:08:14 +0200

    feat: some tests

---
+++ leon/src/main/java/de/jplag/leon/Main.java
@@ -0,0 +1,47 @@
+package de.jplag.leon;
+
+import de.jplag.JPlag;
+import de.jplag.JPlagResult;
+import de.jplag.Language;
+import de.jplag.exceptions.ExitException;
+import de.jplag.java.JavaLanguage;
+import de.jplag.java_cpg.JavaCpgLanguage;
+import de.jplag.options.JPlagOptions;
+import de.jplag.reporting.reportobject.ReportObjectFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Set;
+
+/**
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, JPlag Leon!");
+
+        Language language = new JavaCpgLanguage();
+
+        File submissionsRoot = new File("leon/submissions");
+        Set<File> submissionDirectories = Set.of(submissionsRoot);
+//        File baseCode = new File("leon/baseCode");
+
+        JPlagOptions options = new JPlagOptions(language, submissionDirectories, Set.of());
+//        if (baseCode.exists()) {
+//            options = options.withBaseCodeSubmissionDirectory(baseCode);
+//        }
+
+        try {
+            JPlagResult result = JPlag.run(options);
+            File outDir = new File("leon/output");
+            //File outDir = new File(System.getProperty("user.home") + "/Downloads/");
+            ReportObjectFactory reportObjectFactory = new ReportObjectFactory(outDir);
+            reportObjectFactory.createAndSaveReport(result);
+            System.out.println("JPlag analysis finished. Report written to: " + outDir.getAbsolutePath());
+        } catch (ExitException e) {
+            System.err.println("JPlag exited with an error: " + e.getMessage());
+        } catch (FileNotFoundException e) {
+            System.err.println("I/O error: " + e.getMessage());
+        }
+    }
+}
