5fe88cfcdbdd88fdd80525558d603f558841b52c
LundiNord <git@nyxnord.de>
2026-01-06 14:08:13 +0100

    feat: added removal of dead classes/methods

--- languages/java-cpg/src/main/java/de/jplag/java_cpg/ai/AiPass.kt
+++ languages/java-cpg/src/main/java/de/jplag/java_cpg/ai/AiPass.kt
@@ -3,10 +3,18 @@ package de.jplag.java_cpg.ai
 import de.fraunhofer.aisec.cpg.TranslationContext
 import de.fraunhofer.aisec.cpg.TranslationResult
 import de.fraunhofer.aisec.cpg.graph.Component
+import de.fraunhofer.aisec.cpg.graph.declarations.Declaration
+import de.fraunhofer.aisec.cpg.graph.declarations.MethodDeclaration
+import de.fraunhofer.aisec.cpg.graph.declarations.RecordDeclaration
+import de.fraunhofer.aisec.cpg.graph.declarations.TranslationUnitDeclaration
 import de.fraunhofer.aisec.cpg.graph.records
 import de.fraunhofer.aisec.cpg.passes.TranslationResultPass
 import de.fraunhofer.aisec.cpg.passes.configuration.DependsOn
 import de.jplag.java_cpg.passes.CpgTransformationPass
+import de.jplag.java_cpg.transformation.matching.edges.CpgNthEdge
+import de.jplag.java_cpg.transformation.matching.edges.Edges.RECORD_DECLARATION__METHODS
+import de.jplag.java_cpg.transformation.matching.edges.Edges.TRANSLATION_UNIT__DECLARATIONS
+import de.jplag.java_cpg.transformation.operations.RemoveOperation
 import java.net.URI
 
 /**
@@ -50,6 +58,10 @@ class AiPass(ctx: TranslationContext) : TranslationResultPass(ctx) {
                         visitedLinesRecorder.checkIfCompletelyDead(fileName, startLine, endLine)
                     if (completelyDead) {
                         println("Dead code (class) detected: ${recordDeclaration.name}")
+                        val index = translationUnit.declarations.indexOf(recordDeclaration)
+                        val edge =
+                            CpgNthEdge<TranslationUnitDeclaration, Declaration>(TRANSLATION_UNIT__DECLARATIONS, index)
+                        RemoveOperation.apply(translationUnit, recordDeclaration, edge, true)
                         continue
                     }
                     for (method in recordDeclaration.methods) {
@@ -59,6 +71,10 @@ class AiPass(ctx: TranslationContext) : TranslationResultPass(ctx) {
                             visitedLinesRecorder.checkIfCompletelyDead(fileName, startLine, endLine)
                         if (methodCompletelyDead) {
                             println("Dead code (method) detected: ${method.name} in class ${recordDeclaration.name}")
+                            val index = recordDeclaration.methods.indexOf(method)
+                            val edge =
+                                CpgNthEdge<RecordDeclaration, MethodDeclaration>(RECORD_DECLARATION__METHODS, index)
+                            RemoveOperation.apply(recordDeclaration, method, edge, true)
                         }
                     }
                 }
